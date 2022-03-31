250d249ad870ed65bb6d971c36c33909679929c0
TimErmolt <60045066+TimErmolt@users.noreply.github.com>
2022-03-31 15:53:26 +0300

    feat(repository): creates the repository

---
+++ GamepadControl/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace GamepadControl
+{
+    public partial class Form1 : Form
+    {
+        private Joystick gamepad;
+        private bool[] gamepad_buttons;
+        public Form1()
+        {
+            InitializeComponent();
+            gamepad = new Joystick(this.Handle);
+            ConnectJoystick(gamepad);
+        }
+        private void ConnectJoystick(Joystick joystick)
+        {
+            string joysticks;
+            do
+            {
+                joysticks = joystick.FindJoysticks();
+            }
+            while (joysticks == null);
+
+            if (!joystick.AcquireJoystick(joysticks))
+            {
+                throw new ArgumentException("Gamepad could not be acquired.");
+            }
+
+            EnableTimer();
+        }
+
+        private void EnableTimer()
+        {
+            if (this.InvokeRequired)
+            {
+                BeginInvoke(new ThreadStart(delegate ()
+                {
+                    gamepad_timer.Enabled = true;
+                }));
+            }
+            else
+                gamepad_timer.Enabled = true;
+        }
+
+        private void gamepad_timer_Tick_1(object sender, EventArgs e)
+        {
+            try
+            {
+                gamepad.UpdateStatus();
+                gamepad_buttons = gamepad.buttons;
+
+                if (gamepad_buttons[5])
+                {
+                    textBox1.Text = "1";
+                }
+                else
+                {
+                    textBox1.Text = "0";
+                }
+
+                textBox2.Text = Convert.ToString(gamepad.Yaxis);
+                textBox3.Text = Convert.ToString(gamepad.Xaxis);
+            }
+            catch
+            {
+                gamepad_timer.Enabled = false;
+                ConnectJoystick(gamepad);
+            }
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ GamepadControl/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GamepadControl
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Form1 display = new Form1();
+            Application.Run(display);
+            /*
+            Joystick gamepad = new Joystick();
+            string joysticks = null;
+            
+            do
+            {
+                joysticks = gamepad.FindJoysticks();
+            }
+            while (joysticks == null);
+            
+            if(!gamepad.AcquireJoystick(joysticks))
+            {
+                throw new ArgumentException("Gamepad could not be acquired.");
+            }
+            
+            while(true)
+            {
+                gamepad.UpdateStatus();
+
+                if(gamepad.buttons[5])
+                {
+                    display.textBox1.Text = "1";
+                }
+                else
+                {
+                    display.textBox1.Text = "0";
+                }
+
+                display.textBox2.Text = Convert.ToString(gamepad.Yaxis);
+                display.textBox3.Text = Convert.ToString(gamepad.Xaxis);
+
+            }
+            */
+        }
+    }
+}
